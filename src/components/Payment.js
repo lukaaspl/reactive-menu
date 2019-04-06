ae8b9fc312d39ba014b25e0ca5b319c338257922
Lukasz Roman <lukaaspl@gmail.com>
2019-04-06 22:53:50 +0200

    add "in progress" msg and .htaccess file

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 const Payment = props => {
+    const styles = {
+        h1: {
+            textAlign: 'center',
+            margin: '20px 0 10px',
+        },
+
+        p: {
+            textAlign: 'center',
+            fontSize: 20,
+        },
+
+        sign: {
+            fontSize: 23,
+            marginRight: 5,
+            verticalAlign: -1,
+        }
+    }
+
     return (
         <React.Fragment>
-            <h1>Opłać swoje zamówienie</h1>
+            <h1 style={styles.h1}>Opłać swoje zamówienie</h1>
+
+            {/* in progress */}
+            <p style={styles.p}><i className="fas fa-exclamation-triangle" style={styles.sign}></i> Aplikacja w trakcie budowy, wróć później...</p>
+
             <a href="#" class="back-to-order" onClick={props.menuView}>Wróć do zamówienia</a>
         </React.Fragment>
     )
